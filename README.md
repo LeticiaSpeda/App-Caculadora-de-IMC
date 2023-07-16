daf425a38d7e83e7586bd04fa72206b5bc820eec
Leticia Speda <leticiaspeda@gmail.com>
2023-07-15 21:10:49 -0300

    chore: criado readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# App-Somatator2021
+# App Calculadora de IMC
+
+### Esse app tem o objetivo de calcular o seu IMC baseado em sua altura e peso.
+
+----
+###### aplicativo criado para estudos em Kotlin
+
+
+
+![img.png](img.png)
--- app/src/main/java/com/example/somatator2021/MainActivity.kt
+++ app/src/main/java/com/example/somatator2021/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.somatator2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCalcular = findViewById<Button>(R.id.btnCalcular)
+        val edtPeso = findViewById<EditText>(R.id.edtPeso)
+        val edtAltura = findViewById<EditText>(R.id.edtAltura)
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+
+
+        btnCalcular.setOnClickListener {
+            val peso = edtPeso.text.toString().toDouble();
+            val altura = edtAltura.text.toString().toDouble();
+
+            val resultado = peso / altura * altura
+
+            tvResultado.text = "Seu IMC é ${resultado}"
+
+        }
+
+
     }
 }
